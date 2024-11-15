7d6a1925ad2410db078d477539020dec79fa0d2e
Samy <samy@Samys-MacBook-Air.local>
2024-11-15 10:21:51 +0530

    Addded Program.cs exclusion

--- specmatic-order-api-csharp/Program.cs
+++ specmatic-order-api-csharp/Program.cs
@@ -4,48 +4,57 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using specmatic_order_api_csharp.services;
 using Microsoft.OpenApi.Models;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddEndpointsApiExplorer();  // Add for OpenAPI/Swagger generation
-builder.Services.AddSwaggerGen(c =>
+using System.Diagnostics.CodeAnalysis;
+[ExcludeFromCodeCoverage] // Exclude the entire class from code coverage
+public class Program
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Order API", Version = "v1" });
-    c.MapType<IFormFile>(() => new OpenApiSchema
+    public static void Main(string[] args)
     {
-        Type = "string",
-        Format = "binary"
-    });
-});
+
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddEndpointsApiExplorer(); // Add for OpenAPI/Swagger generation
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Order API", Version = "v1" });
+            c.MapType<IFormFile>(() => new OpenApiSchema
+            {
+                Type = "string",
+                Format = "binary"
+            });
+        });
 
 // Register your custom services
-builder.Services.AddScoped<OrderService>();
-builder.Services.AddScoped<ProductService>();
+        builder.Services.AddScoped<OrderService>();
+        builder.Services.AddScoped<ProductService>();
 
 // Register controllers
-builder.Services.AddControllers();
+        builder.Services.AddControllers();
 
-var app = builder.Build();
+        var app = builder.Build();
 
 // Enable Swagger UI in development
-if (app.Environment.IsDevelopment())
-{
+        if (app.Environment.IsDevelopment())
+        {
 
-}
-app.UseDeveloperExceptionPage();
-app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Order API V1");
-    c.RoutePrefix = string.Empty; // Optional: Set Swagger UI to the root
-});
-app.UseHttpsRedirection();
-app.UseRouting();
+        }
 
-app.UseAuthorization();
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Order API V1");
+            c.RoutePrefix = string.Empty; // Optional: Set Swagger UI to the root
+        });
+        app.UseHttpsRedirection();
+        app.UseRouting();
+
+        app.UseAuthorization();
 
 // This line maps the controllers to their routes
-app.MapControllers();
+        app.MapControllers();
 
-app.Run();
+        app.Run();
+    }
+}
